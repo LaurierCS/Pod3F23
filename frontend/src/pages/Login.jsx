9fbca265b21ad1b5e06e624fef5d8821f0e0ec9a
Usama Mohiuddin <usamamohiudin@gmail.com>
2023-11-23 23:36:37 -0500

    changed files to jsx, added icons in footer, added review and courses card, changed website background

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm/LoginForm'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  )
+}
+
+export default Login
